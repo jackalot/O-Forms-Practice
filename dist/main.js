5120a4f04e200ab13659f08ee925b23a2caa70e1
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-07 07:44:51 -0500

    country now has a function for validaiton

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,32 @@
-document.querySelector(".submit").addEventListener("click",(()=>{console.log("click"),function(){const e=document.querySelector(".email");e.checkValidity()||(document.querySelector(".email-error").textContent=e.validationMessage)}()}));
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("function validateForm() {\n  function validateEmail() {\n    const email = document.querySelector('.email');\n    if (!email.checkValidity()) {\n      document.querySelector('.email-error').textContent = email.validationMessage;\n      return false;\n    }\n    document.querySelector('.email-error').textContent = '';\n    return true;\n  }\n  function validateCountry() {\n    const country = document.querySelector('.country');\n    console.log(country.value);\n    if (!country.checkValidity()) {\n      document.querySelector('.country-error').textContent = country.validationMessage;\n      return false;\n    } if (country.value !== 'unitedstates' || country.value !== 'united states') {\n      //    just one country for the sake of simplicity, in an actual form, this would be a for loop\n      //    testing our input against the loop variables.\n      //    I.E: if(country.value.toLowerCase() === countries[i])\n      document.querySelector('.country-error').textContent = 'enter a valid country(only the united states is valid)';\n      //    country.setCustomValidity('enter a valid country(only the united states is valid)');\n      return false;\n    }\n    return true;\n  }\n  const submit = document.querySelector('.submit');\n  submit.addEventListener('click', () => {\n    console.log('click');\n    const email = validateEmail();\n    const country = validateCountry();\n  });\n}\nvalidateForm();\n\n\n//# sourceURL=webpack://O-Forms-Practice/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,32 @@ function validateForm() {
     const email = document.querySelector('.email');
     if (!email.checkValidity()) {
       document.querySelector('.email-error').textContent = email.validationMessage;
+      return false;
     }
+    document.querySelector('.email-error').textContent = '';
+    return true;
+  }
+  function validateCountry() {
+    const country = document.querySelector('.country');
+    console.log(country.value);
+    if (!country.checkValidity()) {
+      document.querySelector('.country-error').textContent = country.validationMessage;
+      return false;
+    } if (country.value !== 'unitedstates' || country.value !== 'united states') {
+      //    just one country for the sake of simplicity, in an actual form, this would be a for loop
+      //    testing our input against the loop variables.
+      //    I.E: if(country.value.toLowerCase() === countries[i])
+      document.querySelector('.country-error').textContent = 'enter a valid country(only the united states is valid)';
+      //    country.setCustomValidity('enter a valid country(only the united states is valid)');
+      return false;
+    }
+    return true;
   }
   const submit = document.querySelector('.submit');
   submit.addEventListener('click', () => {
     console.log('click');
-    validateEmail();
+    const email = validateEmail();
+    const country = validateCountry();
   });
 }
 validateForm();
