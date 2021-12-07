1ea17e201bef6a2a7c7ec953577aec1e48263686
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-07 07:55:41 -0500

    changed order of the if statements and that fixed the validation for country

--- src/index.js
+++ src/index.js
@@ -14,7 +14,12 @@ function validateForm() {
     if (!country.checkValidity()) {
       document.querySelector('.country-error').textContent = country.validationMessage;
       return false;
-    } if (country.value !== 'unitedstates' || country.value !== 'united states') {
+    }
+    if (country.value === 'unitedstates' || country.value === 'united states') {
+      document.querySelector('.country-error').textContent = 'this is ok';
+      return true;
+    }
+    if (country.value !== 'unitedstates' || country.value !== 'united states') {
       //    just one country for the sake of simplicity, in an actual form, this would be a for loop
       //    testing our input against the loop variables.
       //    I.E: if(country.value.toLowerCase() === countries[i])
@@ -22,7 +27,6 @@ function validateForm() {
       //    country.setCustomValidity('enter a valid country(only the united states is valid)');
       return false;
     }
-    return true;
   }
   const submit = document.querySelector('.submit');
   submit.addEventListener('click', () => {
